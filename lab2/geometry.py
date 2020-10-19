142bb06db289a7c1b4ec9a7e4d29416135448c30
Jake Underland <jaked0626@cs.uchicago.edu>
2020-10-19 05:41:01 -0500

    did geometry

--- lab2/geometry.py
+++ lab2/geometry.py
@@ -4,9 +4,32 @@ import math
 
 # Your distance function goes here 
 
+def dist(a, b):
+    '''
+    Input two vertices a, b which represent points with coordinates 
+    a = [x1, y1] and b = [x2, y2] respectively.
+    The function will compute the distance between the two points. 
+
+    Input: vertices a, b where a = [x1, y1], b = [x2, y2] where contents of vertices are floats.
+    Output: distance between two points. 
+    '''
+    distance = math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+    return distance
+
+
 # Your perimeter function goes here 
 
+def peri(a, b, c):
+    '''
+    Input three vertices a, b, c which represent points with 
+    coordinates (x1, y1), (x2, y2), and (x3, y3) respectively.
+    The function will compute sum of the distance between the three points.
+    '''
+    perimeter = dist(a, b) + dist(a, c) + dist(b, c)
+    return perimeter
+
 def go():
+
     '''
     Write a small amount of code to verify that your functions work
 
