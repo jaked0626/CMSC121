ba6f6e33fd8d0df30038540d7a5ae7731dc56a4d
Jake Underland <jaked0626@cs.uchicago.edu>
2020-11-29 07:39:38 -0600

    finished se6

--- se6/se6.py
+++ se6/se6.py
@@ -15,8 +15,12 @@ def sum_cubes(n):
     This function may not use any loops or list
     comprehensions.
     """
+    if n == 1:
+        return 1
+    else:
+        return n ** 3 + sum_cubes(n-1)
 
-    pass
+
 
 
 def sublists(lst):
@@ -29,8 +33,21 @@ def sublists(lst):
     Returns: (list of list of values) list of all
         sublists of lst.
     """
+    copy_lst = lst[:]
+    if len(copy_lst) == 0:
+        sublst = [[]]
+        return sublst
+    else:
+        x = [copy_lst.pop(0)]
+        sublst_minus_x = sublists(copy_lst)
+        sublst = sublst_minus_x[:]
+        
+        for v_lst in sublst_minus_x:
+            new_lst = x + v_lst
+            sublst.append(new_lst)
+            
+        return sublst
 
-    pass
 
 
 def min_depth_leaf(tree):
@@ -44,8 +61,13 @@ def min_depth_leaf(tree):
     Returns: (integer) the minimum depth of of a leaf
         in tree.
     """
+    if tree.num_children() == 0:
+        return 0
+    else: 
+        lst = [min_depth_leaf(child) for child in tree.children] # returns minimum depth of each child.
+        return min(lst) + 1
 
-    pass
+
 
 
 def repeated_value(tree):
@@ -60,8 +82,12 @@ def repeated_value(tree):
     node in the tree that has an ancestor with the 
     same value.
     """
-    
-    pass
+    boolean = False
+    for child in tree.children:
+        if repeated_value_r(child, set([tree.value])):
+            boolean = True
+            break
+    return boolean
 
 
 def repeated_value_r(tree, ancestor_values):
@@ -83,5 +109,19 @@ def repeated_value_r(tree, ancestor_values):
         in the input tree that has an ancestor in the
         original tree with the same value.
     """
-    
-    pass
+    if tree.value in ancestor_values:
+        return True
+    else:
+        if tree.num_children() > 0:
+            boolean = False
+            new_ancestors = ancestor_values.copy()
+            new_ancestors.add(tree.value)
+            for child in tree.children:
+                if repeated_value_r(child, new_ancestors):
+                    boolean = True
+                    break
+            return boolean
+        else: 
+            return False
+        
+
