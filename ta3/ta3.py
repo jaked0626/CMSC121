568d7e04592547aaaee598c74df25169409e2716
Jake Underland <jaked0626@cs.uchicago.edu>
2020-12-11 09:11:01 -0600

    ta3

---
+++ ta3/ta3.py
@@ -0,0 +1,20 @@
+def next_in_pattern(lst):
+    '''
+    Finds the next number in the pattern, assuming the pattern
+    is described by a polynomial whose degree is less than the
+    length of the input list.
+
+    Input:
+        lst: list of numbers (of length at least one)
+
+    Returns: The next number in the pattern.
+    '''
+    next_number = lst[-1]
+    finite_dif = []
+    for i, _ in enumerate(lst[:-1]):
+        delta = lst[i+1] - lst[i]
+        finite_dif.append(delta)
+    if finite_dif[0] == finite_dif[1]:
+        return next_number + finite_dif[-1]
+    else: 
+        return finite_dif[-1] + next_in_pattern(finite_dif)
